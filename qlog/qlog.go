3ca936db942d06f0cddba5c4941aedb5c90a4770
Marten Seemann <martenseemann@gmail.com>
2020-01-21 13:21:49 +0700

    add the packet_sent event

--- qlog/qlog.go
+++ qlog/qlog.go
@@ -2,14 +2,17 @@ package qlog
 
 import (
 	"io"
+	"time"
 
 	"github.com/francoispqt/gojay"
 	"github.com/lucas-clemente/quic-go/internal/protocol"
+	"github.com/lucas-clemente/quic-go/internal/wire"
 )
 
 // A Tracer records events to be exported to a qlog.
 type Tracer interface {
 	Export(io.Writer) error
+	SentPacket(time.Time, *wire.ExtendedHeader, []wire.Frame)
 }
 
 type tracer struct {
@@ -42,4 +45,19 @@ func (t *tracer) Export(w io.Writer) error {
 			},
 		}})
 	return nil
+}
+
+func (t *tracer) SentPacket(time time.Time, hdr *wire.ExtendedHeader, frames []wire.Frame) {
+	fs := make([]frame, len(frames))
+	for i, f := range frames {
+		fs[i] = *transformFrame(f)
+	}
+	t.events = append(t.events, event{
+		Time: time,
+		eventDetails: eventPacketSent{
+			PacketType: getPacketType(hdr),
+			Header:     *transformHeader(hdr),
+			Frames:     fs,
+		},
+	})
 }
--- qlog/qlog_test.go
+++ qlog/qlog_test.go
@@ -3,8 +3,10 @@ package qlog
 import (
 	"bytes"
 	"encoding/json"
+	"time"
 
 	"github.com/lucas-clemente/quic-go/internal/protocol"
+	"github.com/lucas-clemente/quic-go/internal/wire"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -40,5 +42,54 @@ var _ = Describe("Tracer", func() {
 		Expect(trace).To(HaveKey("vantage_point"))
 		vantagePoint := trace["vantage_point"].(map[string]interface{})
 		Expect(vantagePoint).To(HaveKeyWithValue("type", "server"))
+	})
+
+	Context("Events", func() {
+		exportAndParse := func() (time.Time, string /* category */, string /* event */, map[string]interface{}) {
+			buf := &bytes.Buffer{}
+			Expect(tracer.Export(buf)).To(Succeed())
+
+			m := make(map[string]interface{})
+			Expect(json.Unmarshal(buf.Bytes(), &m)).To(Succeed())
+			Expect(m).To(HaveKey("traces"))
+			traces := m["traces"].([]interface{})
+			Expect(traces).To(HaveLen(1))
+			trace := traces[0].(map[string]interface{})
+			Expect(trace).To(HaveKey("events"))
+			events := trace["events"].([]interface{})
+			Expect(events).To(HaveLen(1))
+			ev := events[0].([]interface{})
+			Expect(ev).To(HaveLen(4))
+			return time.Unix(0, int64(1e6*ev[0].(float64))), ev[1].(string), ev[2].(string), ev[3].(map[string]interface{})
+		}
+
+		It("records a sent packet", func() {
+			now := time.Now()
+			tracer.SentPacket(
+				now,
+				&wire.ExtendedHeader{
+					Header: wire.Header{
+						IsLongHeader:     true,
+						Type:             protocol.PacketTypeHandshake,
+						DestConnectionID: protocol.ConnectionID{1, 2, 3, 4, 5, 6, 7, 8},
+						SrcConnectionID:  protocol.ConnectionID{4, 3, 2, 1},
+						Version:          protocol.VersionTLS,
+					},
+					PacketNumber: 1337,
+				},
+				[]wire.Frame{
+					&wire.MaxStreamDataFrame{StreamID: 42, ByteOffset: 987},
+					&wire.StreamFrame{StreamID: 123, Offset: 1234, Data: []byte("foobar"), FinBit: true},
+				},
+			)
+			t, category, eventName, ev := exportAndParse()
+			Expect(t).To(BeTemporally("~", now, time.Millisecond))
+			Expect(category).To(Equal("transport"))
+			Expect(eventName).To(Equal("packet_sent"))
+			Expect(ev).To(HaveKeyWithValue("packet_type", "handshake"))
+			Expect(ev).To(HaveKey("header"))
+			Expect(ev).To(HaveKey("frames"))
+			Expect(ev["frames"].([]interface{})).To(HaveLen(2))
+		})
 	})
 })
